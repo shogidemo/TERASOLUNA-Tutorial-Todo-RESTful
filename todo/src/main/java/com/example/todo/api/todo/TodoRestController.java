7ea0b99914417a4e30f4ef5c7f792f2639b66cc1
SATO Sho <shogidemo@gmail.com>
2023-04-28 14:05:10 +0900

    一旦コミット

---
+++ todo/src/main/java/com/example/todo/api/todo/TodoRestController.java
@@ -0,0 +1,39 @@
+package com.example.todo.api.todo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.todo.domain.model.Todo;
+import com.example.todo.domain.service.todo.TodoService;
+import com.github.dozermapper.core.Mapper;
+
+@RestController
+@RequestMapping("todos")
+public class TodoRestController {
+
+    @Inject
+    TodoService todoService;
+    @Inject
+    Mapper beanMapper;
+
+    @GetMapping // (1)
+    @ResponseStatus(HttpStatus.OK) // (2)
+    public List<TodoResource> getTodos() {
+        Collection<Todo> todos = todoService.findAll();
+        List<TodoResource> todoResources = new ArrayList<>();
+        for (Todo todo : todos) {
+            todoResources.add(beanMapper.map(todo, TodoResource.class)); // (3)
+        }
+        return todoResources; // (4)
+    }
+
+}
